56ac4330b6ccf9cc8dc46760e55de64f81ca88ba
Uli Heller <uli.heller@daemons-point.com>
2022-02-19 08:41:45 +0100

    gpx-clean.sh

--- bin/gpx-clean.sh
+++ bin/gpx-clean.sh
@@ -1,8 +1,29 @@
 #!/bin/sh
+#
+# https://oleb.net/2020/sanitizing-gpx/
+#
 D="$(dirname "$0")"
 D="$(cd "${D}" && pwd)"
 DD="$(dirname "${D}")"
+BN="$(basename "$0")"
 
+RC=0
+
+TMPDIR="/tmp/${BN}.$({ date; ps waux; }|sha256sum|cut -f1 -d" ").$$~"
+
+cleanUp () {
+    rm -rf "${TMPDIR}"
+    exit "${RC}"
+}
+
+trap cleanUp 0 1 2 3 4 5 6 7 8 9 10 12 13 14 15
+
+mkdir "${TMPDIR}"
+
+#
+# Oben links:   48.871654 - 9.182696
+# Unten rechts: 48.867844 - 9.189369
+#
 xmlstarlet ed \
   -d "//_:extensions" \
   -d "/_:gpx/_:metadata/_:time" \
@@ -11,9 +32,22 @@ xmlstarlet ed \
   -d "//_:trkpt/_:hdop" \
   -d "//_:trkpt/_:vdop" \
   -d "//_:trkpt/_:pdop" \
-  -u "/_:gpx/@creator" -v "Shell script" \
+  -u "/_:gpx/@creator" -v "gpx-clean" \
   "$1" \
   | xmlstarlet tr "${DD}/etc/remove-unused-namespaces.xslt" - \
   | xmlstarlet ed -u "/_:gpx/@xsi:schemaLocation" -v "http://www.topografix.com/GPX/1/1 http://www.topografix.com/GPX/1/1/gpx.xsd" \
-  | xmllint --c14n11 --pretty 2 -
-  
+  | xmllint --c14n11 --pretty 2 - >"${TMPDIR}/clean.gpx"
+
+grep -v "^\s*#" "${DD}/etc/gpx-clean.conf"\
+    |while IFS=":" read lat lon; do
+      lat_min="$(echo "${lat}"|cut -d"-" -f1)"
+      lat_max="$(echo "${lat}"|cut -d"-" -f2)"
+      lon_min="$(echo "${lon}"|cut -d"-" -f1)"
+      lon_max="$(echo "${lon}"|cut -d"-" -f2)"
+      xmlstarlet ed -d "//_:trkpt[@lat>${lat_min} and @lat<${lat_max} and @lon>${lon_min} and @lon<${lon_max}]" "${TMPDIR}/clean.gpx" >"${TMPDIR}/clean-pos.gpx"
+      mv "${TMPDIR}/clean-pos.gpx" "${TMPDIR}/clean.gpx"
+    done
+RC=$?
+
+cat "${TMPDIR}/clean.gpx"
+cleanUp
