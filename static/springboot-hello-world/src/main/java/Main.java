c9531616861e2b46acb5cfc048a26c502d819ddf
Uli Heller <uli.heller@daemons-point.com>
2024-12-29 16:51:16 +0100

    Java-Beispiel 'springboot-hello-world' ausgelagert - Löschen

--- static/springboot-hello-world/src/main/java/Main.java
+++
@@ -1,9 +0,0 @@
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!"); 
-    }
-}
